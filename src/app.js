9ca2bc11954470ad932b30d3d33d2fa451eacffb
asolermaria <asolermaria@gmail.com>
2026-05-10 19:03:11 +0200

    feat/add new dependencies + Swagger documentation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 // Importamos dependencias
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require("express"); // Librería para crear servidores y APIs
+const cookieParser = require("cookie-parser"); // Permite leer las cookies del navegador/Postman dentro de req.cookies
+const morgan = require("morgan"); // Obtención de logs detallados de las llamadas a la API
+const helmet = require("helmet"); // Añade protecciones de seguridad HTTP automáticamente
 require("dotenv").config();
+
+// Documentación Swagger
+const swaggerUi = require("swagger-ui-express"); // Librería con una interfaz visual para la documentación con los endpoints
+const YAML = require("yamljs"); // Permite leer el archivo openapi.yaml y convertirlo en un objeto js
+const path = require("path"); // Permite crear rutas de archivos correctamente y de forma segura independientemente del sistema operativo
+
+const swaggerDocument = YAML.load( // Lee el archivo openapi.yaml y lo convierte en un objeto JavaScript usable por Swagger
+  path.join(process.cwd(), "docs", "openapi.yaml"), // process.cwd() busca desde la raiz del proyecto
+);
 
 // Importamos rutas
 const authRoutes = require("./routes/authRoutes")
@@ -16,18 +27,35 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Usamos dependencias
-app.use(express.json());
+app.use(express.json()); // Permitir a express leer JSON enviado en el body de las peticiones
+app.use(express.urlencoded({ extended: false })); // Permitir a express leer formularios HTML
 app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  helmet({
+    contentSecurityPolicy: { // Estas son las reglas de seguridad sobre qué contenido puedes cargar
+      directives: {
+        "default-src": ["'self'"], // Por defecto solo permite recursos del propio backend/frontend
+        "img-src": [
+          "'self'", // Permite imágenes de tu propia app ("/images/pizza.jpg")
+          "data:", // Permite imágenes base64, que es una imagen convertida en texto e incrustada directamente dentro del HTML/CSS/JSON
+          "https:" // Permite imágenes externas ("https://images.unsplash.com/pizza.jpg")
+        ],
+      },
+    },
+  })
+);
 
 // Usamos rutas
 app.use("/api/auth", authRoutes)
 app.use("/api/recipes", recipeRoutes)
 app.use("/api/favorites", favoriteRoutes)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Prueba funcionamiento server
 app.get("/", (req, res) => {
   res.status(200).json({
-    status: "ok",
+    status: "OK",
     message: "Server running",
     uptime: process.uptime(), // Tiempo que el servidor lleva levantado
     timeStamp: new Date().toISOString() // ISO lectura de máquinas 2026-04-22 UTM
